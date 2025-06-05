f124ece64fbf0a4939c7bfdeead3c1a720d9aa9d
Faisal <pros3sa@gmail.com>
2025-06-05 08:25:06 +0000

    feat: add shared folder

---
+++ src/app/shared/highlight-overdue.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightOverdueDirective } from './highlight-overdue.directive';
+
+describe('HighlightOverdueDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightOverdueDirective();
+    expect(directive).toBeTruthy();
+  });
+});
