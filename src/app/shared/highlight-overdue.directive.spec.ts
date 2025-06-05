39c3911cabe83c8f483891e11bc8c529bcac7dff
Faisal <pros3sa@gmail.com>
2025-06-05 09:47:49 +0000

    chore: arrange files properly

--- src/app/shared/highlight-overdue.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightOverdueDirective } from './highlight-overdue.directive';
-
-describe('HighlightOverdueDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightOverdueDirective();
-    expect(directive).toBeTruthy();
-  });
-});
